8a2e1303c6e05ff5f1db1e1413334bcd7e9c6ff0
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-07-15 16:59:48 +0100

    select_experiment
    Added support for selecting relevant studies with select_experiment.

---
+++ R/select_experiment.R
@@ -0,0 +1,35 @@
+#' Expand nested tables into their own tibbles in the environment
+#' @param project 'dotstask' or 'datequiz'
+#' @param f filter function like \code{function(x) dplyr::filter(x, ...)}
+#'   specifying filter conditions
+#' @param envir environment to write to
+#' @importFrom tibble as_tibble tibble
+#' @importFrom dplyr %>% bind_rows across summarise select
+#' @export
+select_experiment <- function(project, f = function(x) dlpyr::filter(x), envir = .GlobalEnv) {
+  tada(project, package = 'esmData')
+
+  assign('D', get(project))
+
+  D <- f(D)
+
+  # Produce summary table
+  assign(
+    project,
+    D %>%
+      select(-url, -data) %>%
+      summarise(across(-table), tables = paste0(table, collapse = ';')) %>%
+      unique(),
+    envir = envir
+  )
+
+  for (i in 1:nrow(D)) {
+    n <- D$table[i]
+    if (n %in% ls(envir = envir)) {
+      # Try to merge dataframes
+      assign(n, bind_rows(get(n, envir = envir), D$data[i]), envir = envir)
+    } else {
+      assign(n, D$data[i], envir = envir)
+    }
+  }
+}
