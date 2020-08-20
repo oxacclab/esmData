2d84fdf25dbc4877212cf1a791aed47b69d34e61
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-08-20 10:57:57 +0100

    annotate_responsese binary
    Binary dates responses annotation variables

--- R/annotate_responses.R
+++ R/annotate_responses.R
@@ -1,11 +1,17 @@
 #' Add variables assessing the correctness, influence etc. of estimates
 #' @param X tbl of initial estimates and final decisions made with
 #'   advice
-#' @return \code{X} with variables:
+#'
+#' @return \code{X} with variables (*=binary only, ^=continuous only):
 #'
 #' \itemize{
 #' \item{[reponse/advisor#]Correct[Final]}{whether the estimate was correct}
-#' \item{[reponse/advisor#]Error[Final]}{absolute difference between the correct answer and the centre of the estimate}
+#' \item{[reponse/advisor#]Error[Final]^}{absolute difference between the correct answer and the centre of the estimate}
+#' \item{EstimateIncrease}{Difference between final and initial estimate}
+#' \item{Influence}{Difference between final and initial estimate signed in the direction of the advice}
+#' \item{InfluenceCapped*}{Influence capped by the maximum potential increase}
+#' \item{WOAraw^}{Weight on advice = (final - initial) / (advice - initial)}
+#' \item{WOA^}{Winzorized version of WoA Raw to range [0-1]}
 #' }
 #'
 #' @export
@@ -15,29 +21,34 @@ annotate_responses <- function(X) {
   if ('responseEstimateLeft' %in% names(X)) {
     X <- annotate_responses.AdvisedTrial(X)
     changed <- T
-  } else {
-    if ('initialAnswer' %in% names(X)) {
+  }
+  if ('responseAnswerSide' %in% names(X)) {
+    X <- annotate_responses.AdvisedTrial.binary(X)
+    changed <- T
+  }
+  if ('initialAnswer' %in% names(X)) {
       X <- annotate_responses.trials(X)
       changed <- T
-    }
   }
+
   if (!changed)
-    warning('Could not determine appropriate annotation function from names(X).')
+    warning('Could not determine appropriate annotation function by inspecting names().')
   X
 }
 
 #' Add variables assessing the correctness, influence etc. of estimates
 #' @param AdvisedTrial tbl of initial estimates and final decisions made with
 #'   advice in the dates task
+#'
 #' @return \code{AdvisedTrial} with variables:
 #'
 #' \itemize{
-#' \item{[reponse/advisor#]Correct[Final]}{whether the estimate was correct}
-#' \item{[reponse/advisor#]Error[Final]}{absolute difference between the correct answer and the centre of the estimate}
-#' \item{estimateIncrease}{Difference between final and initial estimate}
-#' \item{influence}{Difference between final and initial estimate signed in the direction of the advice}
-#' \item{woaRaw}{Weight on advice = (final - initial) / (advice - initial)}
-#' \item{woa}{Winzorized version of WoA Raw to range [0-1]}
+#' \item{[reponse/advisor#]Correct[Final]}{whether the chosen side was correct}
+#' \item{[reponse/advisor#]Error[Final]}{absolute difference between the correct answer and the centre of the chosen side}
+#' \item{EstimateIncrease}{Difference between final and initial estimate}
+#' \item{Influence}{Difference between final and initial estimate signed in the direction of the advice}
+#' \item{WOAraw}{Weight on advice = (final - initial) / (advice - initial)}
+#' \item{WOA}{Winzorized version of WoA Raw to range [0-1]}
 #' }
 annotate_responses.AdvisedTrial <- function(AdvisedTrial) {
   AdvisedTrial <- mark_responses(AdvisedTrial)
@@ -45,22 +56,47 @@ annotate_responses.AdvisedTrial <- function(AdvisedTrial) {
   AdvisedTrial
 }
 
+#' Add variables assessing the correctness, influence etc. of estimates
+#' @param AdvisedTrial tbl of initial estimates and final decisions made with
+#'   advice in the dates task
+#'
+#' @return \code{AdvisedTrial} with variables:
+#'
+#' \itemize{
+#' \item{[reponse/advisor#]Correct[Final]}{whether the chosen side was correct}
+#' \item{EstimateIncrease}{Difference between final and initial estimate}
+#' \item{Influence}{Difference between final and initial estimate signed in the direction of the advice}
+#' \item{InfluenceCapped}{Influence capped by the maximum potential increase}
+#' }
+annotate_responses.AdvisedTrial.binary <- function(AdvisedTrial) {
+  AdvisedTrial <- mark_responses.binary(AdvisedTrial)
+  AdvisedTrial <- rate_influence.binary(AdvisedTrial)
+  AdvisedTrial
+}
+
 #' Add variables indicating error, correctness, etc. for estimates
 #' @param AdvisedTrial tbl of initial estimates and final decisions made with
 #'   advice
+#'
 #' @return \code{AdvisedTrial} with correct and error columns for initial estimate, final decision, and any advisory estimates
 #' @importFrom dplyr mutate %>% select matches rename_with left_join if_else
 #' @importFrom stringr str_replace str_c str_extract str_ends str_to_sentence
 #' @importFrom tidyr pivot_longer pivot_wider
-#' @importFrom rlang .data
+#' @importFrom rlang .data has_name
 mark_responses <- function(AdvisedTrial) {
+  if (!has_name(AdvisedTrial, 'responseEstimateLeft') |
+      all(is.na(AdvisedTrial$responseEstimateLeft))) {
+    return(AdvisedTrial)
+  }
+
   # Spread into individual estimates
   tmp <- AdvisedTrial %>%
-    select(matches('^(pid|study|timestamp|response|advisor|correct)')) %>%
+    # Add in responseEstimateCentre calculations
     mutate(
       responseEstimateCentre = .data$responseEstimateLeft + .data$responseMarkerWidth / 2,
       responseEstimateCentreFinal = .data$responseEstimateLeftFinal + .data$responseMarkerWidthFinal / 2
     ) %>%
+    select(matches('^(pid|study|timestamp|response|advisor|correct)')) %>%
     rename_with(
       ~ str_replace(., '(advisor[0-9]+)advice(Width)', '\\1Marker\\2'),
       .cols = matches('adviceWidth')
@@ -106,12 +142,56 @@ mark_responses <- function(AdvisedTrial) {
 #' Add variables indicating error, correctness, etc. for estimates
 #' @param AdvisedTrial tbl of initial estimates and final decisions made with
 #'   advice
+#'
+#' @return \code{AdvisedTrial} with correct columns for initial estimate, final decision, and any advisory estimates
+#' @importFrom dplyr mutate %>% select matches rename_with left_join if_else
+#' @importFrom stringr str_replace str_c str_extract str_ends str_to_sentence
+#' @importFrom tidyr pivot_longer pivot_wider
+#' @importFrom rlang .data has_name
+mark_responses.binary <- function(AdvisedTrial) {
+  if (!has_name(AdvisedTrial, 'responseAnswerSide') |
+      all(is.na(AdvisedTrial$responseAnswerSide))) {
+    return(AdvisedTrial)
+  }
+
+  # Spread into individual estimates
+  tmp <- AdvisedTrial %>%
+    select(matches('^(pid|study|timestamp|response|advisor|correct)')) %>%
+    rename_with(
+      ~ str_replace(., '(advisor[0-9]+)advice(Width)', '\\1Marker\\2'),
+      .cols = matches('adviceWidth')
+    ) %>%
+    pivot_longer(matches('(responseAnswerSide|advisor[0-9]+adviceSide)')) %>%
+    mutate(
+      prefix = str_extract(.data$name, '(advisor[0-9]+|response)(Answer|advice)'),
+      suffix = if_else(str_ends(.data$name, 'Final'), 'Final', '')
+    )
+
+  # Mark individual estimates
+  tmp <- tmp %>% mutate(correct = .data$value == .data$correctAnswerSide)
+
+  # Restore wide format
+  tmp <- tmp %>%
+    select(-.data$value, -.data$name) %>%
+    rename_with(str_to_sentence, .cols = .data$correct) %>%
+    pivot_wider(id_cols = matches('^(pid|study|timestamp)'),
+                names_from = c(.data$prefix, .data$suffix),
+                names_glue = "{prefix}SideCorrect{suffix}",
+                values_from = .data$Correct)
+
+  # Join back onto the main tbl
+  left_join(AdvisedTrial, tmp, by = names(tmp)[names(tmp) %in% names(AdvisedTrial)])
+}
+
+#' Add variables indicating error, correctness, etc. for estimates
+#' @param AdvisedTrial tbl of initial estimates and final decisions made with
+#'   advice
 #' @return \code{AdvisedTrial} with additional columns \code{advisor#...} where ... is:
 #' \itemize{
-#' \item{estimateIncrease}{Difference between final and initial estimate}
-#' \item{influence}{Difference between final and initial estimate signed in the direction of the advice}
-#' \item{woaRaw}{Weight on advice = (final - initial) / (advice - initial)}
-#' \item{woa}{Winzorized version of WoA Raw to range [0-1]}
+#' \item{EstimateIncrease}{Difference between final and initial estimate}
+#' \item{Influence}{Difference between final and initial estimate signed in the direction of the advice}
+#' \item{WOAraw}{Weight on advice = (final - initial) / (advice - initial)}
+#' \item{WOA}{Winzorized version of WoA Raw to range [0-1]}
 #' }
 #' @importFrom dplyr rename_with %>% starts_with mutate case_when select
 #' @improtFrom tidyr pivot_longer pivot_wider
@@ -121,7 +201,7 @@ rate_influence <- function(AdvisedTrial) {
   tmp <- AdvisedTrial %>%
     rename_with(~ str_replace(., 'advisor([0-9]+)', 'x\\1'), .cols = matches('advisor[0-9]+$')) %>%
     pivot_longer(
-      starts_with('advisor'),
+      matches('^advisor[0-9]+'),
       names_to = c('advisor', '.value'),
       names_pattern = '(advisor[0-9]+)(.*)'
     )
@@ -131,17 +211,17 @@ rate_influence <- function(AdvisedTrial) {
     mutate(
       .middle = .data$responseEstimateLeft + .data$responseMarkerWidth / 2,
       .middleFinal = .data$responseEstimateLeftFinal + .data$responseMarkerWidthFinal / 2,
-      estimateIncrease = .data$.middleFinal - .data$.middle,
-      influence = case_when(
-        .data$.middle < .data$adviceCentre ~ .data$estimateIncrease,
-        .data$.middle > .data$adviceCentre ~ -.data$estimateIncrease,
+      EstimateIncrease = .data$.middleFinal - .data$.middle,
+      Influence = case_when(
+        .data$.middle < .data$adviceCentre ~ .data$EstimateIncrease,
+        .data$.middle > .data$adviceCentre ~ -.data$EstimateIncrease,
         T ~ NA_real_
       ),
-      woaRaw = (.data$.middleFinal - .data$.middle) / (.data$adviceCentre - .data$.middle),
-      woa = case_when(
-        .data$woaRaw < 0 ~ 0,
-        .data$woaRaw > 1 ~ 1,
-        T ~ .data$woaRaw
+      WOAraw = (.data$.middleFinal - .data$.middle) / (.data$adviceCentre - .data$.middle),
+      WOA = case_when(
+        .data$WOAraw < 0 ~ 0,
+        .data$WOAraw > 1 ~ 1,
+        T ~ .data$WOAraw
       )
     ) %>%
     select(-starts_with('.'))
@@ -156,32 +236,58 @@ rate_influence <- function(AdvisedTrial) {
     rename_with(~ str_replace(., 'x([0-9]+)', 'advisor\\1'), .cols = matches('x[0-9]+$'))
 }
 
-#' Calculate influence for binary advised trials
+#' Add variables indicating error, correctness, etc. for estimates
 #' @param AdvisedTrial tbl of initial estimates and final decisions made with
 #'   advice
-#' @details Adjusted weight on advice (woa) is capped by limiting the maximum
-#'   change where answers are reduced to be equivalent to the maximum possible
-#'   change if answers had been increased. This compensates for the fact that,
-#'   once the initial estimate marks a point to one side of a scale, there is by
-#'   definition a greater room to move towards the other side of the scale than
-#'   there is to increase the rating on the chosen side.
-#'
-#'   The returned tbl has the following additional columns for each advisor
-#'   (\code{AdvisedTrial$advisor#...}):
-#'
-#'   \itemize{
-#'   \item{changedOfMind}{whether the chosen side is the same for the initial estimate and final decision}
-#'   \item{increase}{how much confidence has increased from initial estimate to final decision}
-#'   \item{agrees}{whether the advisor agrees with the initial decision}
-#'   \item{woaRaw}{how much the}
-#'   }
-#' @return \code{AdvisedTrial} with columns appended containing influence
-#'   calculations
-#' @importFrom tibble as_tibble tibble
-#' @importFrom dplyr %>% bind_rows across summarise select
-#' @export
-get_influence_binary <- function(AdvisedTrial) {
+#' @return \code{AdvisedTrial} with additional columns \code{advisor#...} where ... is:
+#' \itemize{
+#' \item{EstimateIncrease}{Difference between final and initial estimate}
+#' \item{Influence}{Difference between final and initial estimate signed in the direction of the advice}
+#' \item{InfluenceCapped}{Influence capped by the maximum potential increase}
+#' }
+#' @importFrom dplyr rename_with %>% starts_with mutate case_when select matches
+#' @improtFrom tidyr pivot_longer pivot_wider
+#' @importFrom rlang .data
+rate_influence.binary <- function(AdvisedTrial) {
+  # Reshape to separate out advisors
+  tmp <- AdvisedTrial %>%
+    rename_with(~ str_replace(., 'advisor([0-9]+)', 'x\\1'), .cols = matches('advisor[0-9]+$')) %>%
+    pivot_longer(
+      matches('^advisor[0-9]+'),
+      names_to = c('advisor', '.value'),
+      names_pattern = '(advisor[0-9]+)(.*)'
+    )
 
+  # Calculate summary values by advisor
+  tmp <- tmp %>%
+    mutate(
+      .max = 100 - .data$responseConfidence,
+      .switch = .data$responseAnswerSide != .data$responseAnswerSideFinal,
+      .agree = .data$responseAnswerSide == .data$adviceSide,
+      EstimateIncrease = if_else(
+        .data$.switch,
+        -(.data$responseConfidenceFinal + .data$responseConfidence),
+        .data$responseConfidenceFinal - .data$responseConfidence
+      ),
+      Influence = case_when(
+        .data$.agree ~ .data$EstimateIncrease,
+        .data$.agree ~ -.data$EstimateIncrease,
+        T ~ NA_real_
+      ),
+      InfluenceCapped = if_else(abs(.data$Influence) > .data$.max,
+                                .data$.max * sign(.data$Influence),
+                                .data$Influence)
+    ) %>%
+    select(-starts_with('.'))
+
+  # Restore original structure
+  pivot_wider(
+    tmp,
+    names_from = .data$advisor,
+    names_glue = "{advisor}{.value}",
+    values_from = (which(names(tmp) == 'advisor') + 1):ncol(tmp)
+  ) %>%
+    rename_with(~ str_replace(., 'x([0-9]+)', 'advisor\\1'), .cols = matches('x[0-9]+$'))
 }
 
 #' Add variables assessing the correctness, influence etc. of estimates
@@ -206,7 +312,7 @@ annotate_responses.trials <- function(trials) {
       finalAnswerCorrect = .data$finalAnswer == .data$correctAnswer,
       adviceCorrect = .data$adviceSide == .data$correctAnswer
     ) %>%
-    # Calculate influence and woa
+    # Calculate influence
     mutate(
       adviceInfluence = case_when(
         initialAnswer == .data$adviceSide & !.data$switch ~
--- tests/testthat/test_datequiz_annotation.R
+++ tests/testthat/test_datequiz_annotation.R
@@ -5,7 +5,9 @@ library(rlang)
 
 expect_silent(select_experiment('datequiz', envir = environment()))
 at <-  filter(AdvisedTrial,
-              .data$pid != '' & .data$pid != '0.1.3' & .data$pid != '1')
+              .data$pid != '' & .data$pid != '0.1.3' & .data$pid != '1' &
+                !(.data$studyId == 'markerUse' & .data$studyVersion == '0.2.1') &
+                !(.data$studyId == 'minGroups' & .data$studyVersion == '1.2.0'))
 x <- annotate_responses(at)
 
 test_that("annotate_responses completes", {
@@ -20,5 +22,11 @@ test_that("annotate_responses adds columns", {
 
 test_that("annotate_responses includes correct and error columns", {
   expect_equal('responseCorrect' %in% names(x), T)
+  expect_equal('responseAnswerSideCorrect' %in% names(x), T)
   expect_equal('responseError' %in% names(x), T)
+})
+
+test_that("annotate_responses includes influence and WOA columns", {
+  expect_equal('advisor0Influence' %in% names(x), T)
+  expect_equal('advisor0WOA' %in% names(x), T)
 })
